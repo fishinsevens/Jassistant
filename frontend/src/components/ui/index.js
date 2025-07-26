cbad21f2c1924d854906e71a82be93abf99ee84a
fishinsevens <34882754+fishinsevens@users.noreply.github.com>
2025-07-27 02:56:13 +0800

    Create index.js

---
+++ frontend/src/components/ui/index.js
@@ -0,0 +1,9 @@
+// 导出所有UI组件
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as FormInput } from './FormInput';
+export { default as Spinner, FullPageSpinner } from './Spinner';
+export { default as Alert } from './Alert';
+
+// 这样可以使用以下方式导入组件:
+// import { Button, Card, FormInput, Spinner, Alert } from '@/components/ui'; 
